482930b850166dde3fa47f3f33ec206ae89a2197
Mats Högberg <mats@hgbrg.se>
2017-02-23 22:05:21 +0100

    .

--- solve.sh
+++ solve.sh
@@ -2,9 +2,9 @@
 
 rm -rf solutions/
 mkdir solutions/
-python3 solution.py inputs/kittens.in > solutions/kittens.txt
-python3 solution.py inputs/me_at_the_zoo.in > solutions/me_at_the_zoo.txt
-python3 solution.py inputs/trending_today.in > solutions/trending_today.txt
-python3 solution.py inputs/videos_worth_spreading.in > solutions/videos_worth_spreading.txt
+python3 best_per_cache.py inputs/kittens.in > solutions/kittens.txt
+python3 best_per_cache.py inputs/me_at_the_zoo.in > solutions/me_at_the_zoo.txt
+python3 best_per_cache.py inputs/trending_today.in > solutions/trending_today.txt
+python3 best_per_cache.py inputs/videos_worth_spreading.in > solutions/videos_worth_spreading.txt
 
-zip solutions/sol.zip parse.py solution.py
+zip solutions/sol.zip parse.py best_per_cache.py tools.py
