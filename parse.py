366c990c96709135acd53e9e723015e8a65dfe95
Edvard Hübinette <edvard@hubinette.me>
2017-02-23 19:57:22 +0100

    Add parser for input

--- parse.py
+++ parse.py
@@ -1,16 +1,49 @@
 
-#params = (rows, cols, topping_limit, cell_limit)
+# Inputs
+# V, E, R, C, X
+# S_1, S_2, ..., S_no_videos
+# E endpoints:
+#   L_D, K
+#   K lines (one per cache)
+#       C (cache id)
+#       L_C (latency from E to K)
+# R lines (request descriptions):
+#   R_v (video), R_e (endpoint), R_n (#request)
+
 def parse(filename):
-    # datatype here
     with open(filename, 'r') as f:
-        rows, cols, topping_limit, cell_limit = map(int, f.readline().split())
-        data = f.read().split("\n")[:-1] # delete the last newline
-        
-        for line in data:
-            pass # insert into datatype
-    
-    # return data
+        V, E, R, C, X = parse_line(f)
+        S = list(map(int, f.readline().split()))
+        endpoints = parse_endpoints(f, E)
+        requests = parse_requests(f, R) 
+        return endpoints, requests
 
+def parse_requests(f_open, R):
+    requests = {}
+    for r in range(R):
+        # for each request description
+        vid_id, endp_id, no_reqs = parse_line(f_open)
+        requests[(vid_id, endp_id)] = no_reqs
+    return requests
 
-def precompute_stuff():
-    pass
+def parse_endpoints(f_open, no_endp):
+    endpoints = []
+    for i in range(no_endp):
+        # for each endpoint
+        latency, no_caches = parse_line(f_open)
+        endpoints.append((latency, {}))
+        for j in range(no_caches):
+            # for each cache connected to this endpoint
+            cache, cache_latency = parse_line(f_open)
+            endpoints[-1][1][cache] = cache_latency
+    return endpoints
+
+def parse_line(f_open):
+    return list(map(int, f_open.readline().split()))
+
+if __name__ == "__main__":
+    endpoints, requests = parse("kittens.in")
+    print("ENDPOINTS: --------")
+    print(endpoints[0])
+    print("REQUEST: ---------")
+    print(requests.popitem())
