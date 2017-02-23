f51730ad74a75410a82ccdc3a77d141adc295671
hjorthjort <rikard.hjort@gmail.com>
2017-02-23 19:21:40 +0000

    Better solving

--- solve.sh
+++ solve.sh
@@ -6,3 +6,5 @@ python3 solution.py inputs/kittens.in > solutions/kittens.txt
 python3 solution.py inputs/me_at_the_zoo.in > solutions/me_at_the_zoo.txt
 python3 solution.py inputs/trending_today.in > solutions/trending_today.txt
 python3 solution.py inputs/videos_worth_spreading.in > solutions/videos_worth_spreading.txt
+
+zip solutions/sol.zip parse.py solution.py
