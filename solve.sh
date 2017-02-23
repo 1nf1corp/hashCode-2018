2f0284c59f40c46d92e55a8edc31c406c9763506
Mats Högberg <mats@hgbrg.se>
2017-02-23 20:15:21 +0100

    Added solve script

---
+++ solve.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf solutions/
+mkdir solutions/
+python3 solution.py inputs/kittens.in > solutions/kittens.txt
+python3 solution.py inputs/me_at_the_zoo.in > solutions/me_at_the_zoo.txt
+python3 solution.py inputs/trending_today.in > solutions/trending_today.txt
+python3 solution.py inputs/videos_worth_spreading.in > solutions/videos_worth_spreading.txt
