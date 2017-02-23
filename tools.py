06e3391a855855611cab954f12cae6bca392508c
Edvard Hübinette <edvard@hubinette.me>
2017-02-23 22:04:48 +0100

    Improve top_videos_endpoint

--- tools.py
+++ tools.py
@@ -1,4 +1,3 @@
-
 def videos_endpoint(endpoint, requests):
     videos = []
     for (video, endp), reqs in requests.items():
@@ -6,15 +5,29 @@ def videos_endpoint(endpoint, requests):
             videos.append((reqs, video))
     return videos
 
+#def top_videos_endpoints(endpoints, requests):
+#    reqs_per_video = {}
+#    for e in endpoints:
+#        videos = videos_endpoint(e, requests)
+#        for reqs, video in videos:
+#            if video in reqs_per_video:
+#                reqs_per_video[video] += reqs
+#            else:
+#                reqs_per_video[video] = reqs
+#    
+#    top_videos = []
+#    for video, reqs in reqs_per_video.items():
+#        top_videos.append((reqs, video))
+#    return sorted(top_videos, reverse=True)
+
 def top_videos_endpoints(endpoints, requests):
     reqs_per_video = {}
-    for e in endpoints:
-        videos = videos_endpoint(e, requests)
-        for reqs, video in videos:
-            if video in reqs_per_video:
-                reqs_per_video[video] += reqs
-            else:
-                reqs_per_video[video] = reqs
+    for (video, _), reqs in requests.items():
+        if video in reqs_per_video:
+            reqs_per_video[video] += reqs
+        else:
+            reqs_per_video[video] = reqs
+    
     top_videos = []
     for video, reqs in reqs_per_video.items():
         top_videos.append((reqs, video))
