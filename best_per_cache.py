85cbf96596e502e32b09dc4c8a3f2add328d567c
hjorthjort <rikard.hjort@gmail.com>
2017-02-23 20:33:44 +0000

    more

--- best_per_cache.py
+++ best_per_cache.py
@@ -1,20 +1,30 @@
 import tools
 
-def solve_per_cache(cache, max_cap, S, req_dict):
-    eps = endpoints_connected_to_cache(cache)
-    # brøds function for getting best videos for a cache
-    pq = top_videos_endpoint(eps, req_dict)
-    idx = 0
-    # As long as we can pick more, do so
-    while (idx < len(pq) and S[pq[idx][1]] <= max_cap):
-        max_cap = max_cap - S[pq[idx]]
-        idx = idx + 1
-    solution = pq[:idx]
-    new_req_dict = remove_vid_from_eps(solution, eps, req_dict)
-    return (solution, new_req_dict)
+def endpoints_connected_to_cache(endpoints):
+    return {0: [1,3], 1: [0,1]}
+
+def solve_per_cache(max_cap, endpoints, S, req_dict):
+    cache_eps = endpoints_connected_to_cache(endpoints)
+    all_solutions = {}
+    for cache, eps in cache_eps.items():
+        # brøds function for getting best videos for a cache
+        pq = tools.top_videos_endpoints(eps, req_dict)
+        idx = 0
+        # As long as we can pick more, do so
+        while (idx < len(pq) and S[pq[idx][1]] <= max_cap):
+            max_cap = max_cap - S[pq[idx][1]]
+            idx = idx + 1
+        solution = pq[:idx]
+        req_dict = remove_vid_from_eps(solution, eps, req_dict)
+        all_solutions[cache] = solution
+    return all_solutions
 
 def remove_vid_from_eps(vids, eps, req_dict):
     for (v, ep) in req_dict.items():
         if v in vids and ep in eps:
             del req_dict[(v,ep)]
     return req_dict
+
+# req_dict = {(0,2): 1000, (2,3): 1500}
+# endpoints = [(4, {0: 1, 1:3}), (10, {}), (3, {0: 2, 1:2}) ]
+# print(solve_per_cache(100, endpoints, [30,100,40,50,80], req_dict))
